f1c780cac903a55867e09313e1b687a047e8402e
Vladyslav Sitalo <sitalov@amazon.com>
2024-12-30 16:48:41 -0800

    Monaco improve jsx handling

--- src/components/RendererBlockRenderer.tsx
+++ src/components/RendererBlockRenderer.tsx
@@ -1,26 +1,35 @@
 import {Editor} from '@monaco-editor/react'
 import {BlockRendererProps} from '../types'
 import {DefaultBlockRenderer} from './DefaultBlockRenderer.tsx'
-import type {editor as MonacoEditor} from 'monaco-editor'
+import type {editor} from 'monaco-editor'
 import {debounce} from 'lodash'
 import {useCallback} from 'react'
 
+type MonacoEditor = editor.IStandaloneCodeEditor
 
 const MonacoContentRenderer = ({block, onUpdate}: BlockRendererProps) => {
-    const onChange = useCallback((value: string) => {
+    const onChange = useCallback((value: string | undefined) => {
         if (value !== undefined && value !== block.content) {
             onUpdate({...block, content: value})
         }
     }, [block, onUpdate])
     const debouncedOnChange = debounce(onChange, 300)
 
-    const handleEditorMount = (editor: MonacoEditor) =>
-        editor.onDidBlurEditorText(() => onChange(editor.getValue()))
+    const handleEditorMount = (editor: MonacoEditor, monaco: any) => {
+        monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+            //todo unclear if this is the correct  option
+            jsx: monaco.languages.typescript.JsxEmit.Preserve,
+        });
 
+        return editor.onDidBlurEditorText(() => onChange(editor.getValue()))
+    }
+
+    // supplying the path is a bit sus bc if there is name collision - monaco would treat these as one editor
     return <Editor
         height="400px"
         defaultLanguage="typescript"
         defaultValue={block.content}
+        defaultPath={`${block.id}.tsx`}
         onChange={debouncedOnChange}
         onMount={handleEditorMount}
         options={{
