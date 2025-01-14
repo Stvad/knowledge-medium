a85be670fb0e9e0f8238e6217dbbf3381dbb1535
Vladyslav Sitalo <sitalov@amazon.com>
2025-01-14 12:12:54 -0800

    Block: outdent/indent

--- src/data/block.ts
+++ src/data/block.ts
@@ -1,76 +1,157 @@
-import {DocHandle, Repo, AutomergeUrl, isValidAutomergeUrl} from '@automerge/automerge-repo'
+import {
+  DocHandle,
+  Repo,
+  AutomergeUrl,
+  isValidAutomergeUrl,
+} from '@automerge/automerge-repo'
 import {BlockData as BlockData, BlockPropertyValue} from '@/types.ts'
 import {ChangeOptions as AutomergeCahngeOptions} from '@automerge/automerge'
 import {createBlockDoc} from '@/utils/block-operations.ts'
 import {useDocument} from '@automerge/automerge-repo-react-hooks'
 
 export type ChangeFn<T> = (doc: T) => void;
-export type ChangeOptions<T> = AutomergeCahngeOptions<T>
+export type ChangeOptions<T> = AutomergeCahngeOptions<T>;
 
 /**
  * I want to abstract away the details of the storage lay away from the component, so i can plug in jazz.tools or similar later
  */
 export class Block {
-    static async new(repo: Repo, data: Partial<BlockData>) {
-        const doc = createBlockDoc(repo, data)
-        return new Block(repo, doc.url)
+  static new(repo: Repo, data: Partial<BlockData>) {
+    const doc = createBlockDoc(repo, data)
+    return new Block(repo, doc.url)
+  }
+
+  id: AutomergeUrl
+  handle: DocHandle<BlockData>
+
+  constructor(
+    readonly repo: Repo,
+    id: string,
+  ) {
+    if (!isValidAutomergeUrl(id)) throw new Error('Invalid block id')
+
+    this.id = id
+    this.handle = repo.find<BlockData>(id)
+  }
+
+  change(
+    callback: ChangeFn<BlockData>,
+    options: ChangeOptions<BlockData> = {},
+  ) {
+    this.handle.change(callback, options)
+  }
+
+  /**
+   * todo we should outdent outside the view point, but that's not something this function can be aware of
+   */
+  outdent() {
+    const doc = this.handle.docSync()
+    if (!doc) throw new Error(`Block not found: ${this.id}`)
+    if (!doc.parentId) return // Already at root level, can't outdent further
+
+    const parent = this.repo.find<BlockData>(doc.parentId as AutomergeUrl)
+    const parentDoc = parent.docSync()
+    if (!parentDoc) throw new Error(`Parent block not found: ${doc.parentId}`)
+    if (!parentDoc.parentId) return // Parent is root, can't outdent further
+
+    const grandparent = this.repo.find<BlockData>(
+      parentDoc.parentId as AutomergeUrl,
+    )
+
+    // 1. Remove this block from current parent's children
+    parent.change((parent) => {
+      const index = [...parent.childIds].indexOf(this.id)
+      parent.childIds.splice(index, 1)
+    })
+
+    // 2. Add this block to grandparent's children after the parent
+    grandparent.change((grandparent) => {
+      const parentIndex = [...grandparent.childIds].indexOf(
+        doc.parentId as AutomergeUrl,
+      )
+      grandparent.childIds.splice(parentIndex + 1, 0, this.id)
+    })
+
+    // 3. Update this block's parentId
+    this.change((doc) => {
+      doc.parentId = parentDoc.parentId
+    })
+  }
+
+  indent() {
+    const doc = this.handle.docSync()
+    if (!doc) throw new Error(`Block not found: ${this.id}`)
+    if (!doc.parentId) return // Can't indent root level block
+
+    const parent = this.repo.find<BlockData>(doc.parentId as AutomergeUrl)
+    const parentDoc = parent.docSync()
+    if (!parentDoc) throw new Error(`Parent block not found: ${doc.parentId}`)
+
+    // Find previous sibling to become new parent
+    const currentIndex = [...parentDoc.childIds].indexOf(this.id)
+    if (currentIndex <= 0) return // No previous sibling, can't indent
+
+    const newParentId = parentDoc.childIds[currentIndex - 1]
+    const newParent = this.repo.find<BlockData>(newParentId as AutomergeUrl)
+
+    // 1. Remove from current parent's children
+    parent.change((parent) => {
+      parent.childIds.splice(currentIndex, 1)
+    })
+
+    // 2. Add to new parent's children
+    newParent.change((newParent) => {
+      newParent.childIds.push(this.id)
+    })
+
+    // 3. Update this block's parentId
+    this.change((doc) => {
+      doc.parentId = newParentId
+    })
+  }
+
+  async createSiblingBelow(data: Partial<BlockData> = {}) {
+    const doc = await this.handle.doc()
+    if (!doc) throw new Error(`Block not found: ${this.id}`)
+    if (!doc.parentId)
+      throw new Error('Cannot create sibling for top-level block')
+
+    const newBlock = Block.new(this.repo, {
+      ...data,
+      parentId: doc.parentId,
+    })
+
+    const parent = this.repo.find<BlockData>(doc.parentId as AutomergeUrl)
+    console.log({parent: parent.docSync()})
+    parent.change((parent) => {
+      // https://github.com/automerge/automerge/pull/717
+      // I should probably go use jazz.tools 😛
+      const index = [...parent.childIds].indexOf(this.id)
+      parent.childIds.splice(index + 1, 0, newBlock.id)
+    })
+
+    return new Block(this.repo, newBlock.id)
+  }
+
+  use() {
+    return useDocument<BlockData>(this.id)[0]
+  }
+
+  useProperty<T extends BlockPropertyValue>(
+    name: string,
+  ): [T | undefined, (value: T) => void];
+  useProperty<T extends BlockPropertyValue>(
+    name: string,
+    initialValue: T,
+  ): [T, (value: T) => void];
+  useProperty<T extends BlockPropertyValue>(name: string, initialValue?: T) {
+    const doc = this.use()
+    const value = (doc?.properties[name] ?? initialValue) as T | undefined
+
+    const setValue = (newValue: T) => {
+      this.change((doc) => (doc.properties[name] = newValue))
     }
 
-    id: AutomergeUrl
-    handle: DocHandle<BlockData>
-
-    constructor(readonly repo: Repo, id: string) {
-        if (!isValidAutomergeUrl(id)) throw new Error('Invalid block id')
-
-        this.id = id
-        this.handle = repo.find<BlockData>(id)
-    }
-
-
-    change(callback: ChangeFn<BlockData>, options: ChangeOptions<BlockData> = {}) {
-        this.handle.change(callback, options)
-    }
-
-    outdent() {
-    }
-
-    indent() {
-    }
-
-    async createSiblingBelow(data: Partial<BlockData>) {
-        const doc = await this.handle.doc()
-        if (!doc) throw new Error(`Block not found: ${this.id}`)
-        if (!doc.parentId) throw new Error('Cannot create sibling for top-level block')
-
-        const newBlock = createBlockDoc(this.repo, {
-            ...data,
-            parentId: doc.parentId,
-        })
-
-        // Insert the new block after this one in the parent's childIds
-        const parent = this.repo.find<BlockData>(doc.parentId as AutomergeUrl)
-        parent.change(parent => {
-            const index = parent.childIds.indexOf(this.id)
-            parent.childIds.splice(index + 1, 0, newBlock.url)
-        })
-
-        return new Block(this.repo, newBlock.url)
-    }
-
-    use() {
-        return useDocument<BlockData>(this.id)[0]
-    }
-
-    useProperty<T extends BlockPropertyValue>(name: string): [T | undefined, (value: T) => void];
-    useProperty<T extends BlockPropertyValue>(name: string, initialValue: T): [T, (value: T) => void];
-    useProperty<T extends BlockPropertyValue>(name: string, initialValue?: T) {
-        const doc = this.use()
-        const value = (doc?.properties[name] ?? initialValue) as T | undefined
-
-        const setValue = (newValue: T) => {
-            this.change(doc => doc.properties[name] = newValue)
-        }
-
-        return [value, setValue]
-    }
+    return [value, setValue]
+  }
 }
