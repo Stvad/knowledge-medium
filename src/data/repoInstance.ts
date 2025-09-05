6076030465658c831474b01bc50425ddfe4d3401
Vladyslav Sitalo <sitalov@amazon.com>
2025-09-04 17:40:52 -0700

    Convex snapshot

--- src/data/repoInstance.ts
+++ src/data/repoInstance.ts
@@ -2,10 +2,17 @@ import { Repo as AutomergeRepo } from '@automerge/automerge-repo'
 import { BrowserWebSocketClientAdapter } from '@automerge/automerge-repo-network-websocket'
 import { IndexedDBStorageAdapter } from '@automerge/automerge-repo-storage-indexeddb'
 import { UndoRedoManager } from '@onsetsoftware/automerge-repo-undo-redo'
+import { ConvexReactClient } from 'convex/react'
+import { sync } from '@/convex/sync.ts'
 
 export const automergeRepo = new AutomergeRepo({
-  network: [new BrowserWebSocketClientAdapter('wss://sync.automerge.org')],
+  // network: [new BrowserWebSocketClientAdapter('wss://sync.automerge.org')],
   storage: new IndexedDBStorageAdapter(),
 })
+
+const convex = new ConvexReactClient("https://dazzling-cobra-717.convex.cloud");
+
+sync(automergeRepo, convex);
+
 
 export const undoRedoManager = new UndoRedoManager()
