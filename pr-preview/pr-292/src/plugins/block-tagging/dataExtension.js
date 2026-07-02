205ff0bf971c00f02194ed8a04531bcf7a38c8fb
Stvad <Stvad@users.noreply.github.com>
2026-07-02 17:22:29 +0000

    Remove preview for PR 292 🛬

--- pr-preview/pr-292/src/plugins/block-tagging/dataExtension.js
+++
@@ -1,9 +0,0 @@
-import { propertySchemasFacet } from "../../data/facets.js";
-import { pluginPrefsExtension } from "../../data/pluginStateExtensions.js";
-import { blockTaggingPrefsType, blockTagsConfigProp } from "./config.js";
-//#region src/plugins/block-tagging/dataExtension.ts
-var blockTaggingDataExtension = [propertySchemasFacet.of(blockTagsConfigProp, { source: "block-tagging" }), ...pluginPrefsExtension(blockTaggingPrefsType, "block-tagging")];
-//#endregion
-export { blockTaggingDataExtension };
-
-//# sourceMappingURL=dataExtension.js.map
