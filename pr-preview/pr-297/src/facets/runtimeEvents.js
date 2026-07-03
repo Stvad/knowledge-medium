1792253423b1abf3ca0651f2399dcc4127f4d212
Stvad <Stvad@users.noreply.github.com>
2026-07-03 20:33:59 +0000

    Remove preview for PR 297 🛬

--- pr-preview/pr-297/src/facets/runtimeEvents.js
+++
@@ -1,9 +0,0 @@
-//#region src/facets/runtimeEvents.ts
-var appRuntimeUpdateEvent = "app-runtime-update";
-var refreshAppRuntime = () => {
-	window.dispatchEvent(new CustomEvent(appRuntimeUpdateEvent, { detail: (/* @__PURE__ */ new Date()).toISOString() }));
-};
-//#endregion
-export { appRuntimeUpdateEvent, refreshAppRuntime };
-
-//# sourceMappingURL=runtimeEvents.js.map
--- pr-preview/pr-297/src/plugins/breadcrumbs/breadcrumbPreview.js
+++
@@ -1,6 +0,0 @@
-//#region src/plugins/breadcrumbs/breadcrumbPreview.ts
-var getBreadcrumbContentPreview = (content) => content.match(/^[^\r\n]*/)?.[0] ?? "";
-//#endregion
-export { getBreadcrumbContentPreview };
-
-//# sourceMappingURL=breadcrumbPreview.js.map
