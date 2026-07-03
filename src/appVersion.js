8bfed3e6f944428753379ee58f12e1323bd88c2e
Stvad <vlad@sitalo.org>
2026-07-03 19:50:42 +0000

    Deploying to gh-pages from @ Stvad/knowledge-medium@628b5086ae14b67abe4b3ece25be18848df72fef 🚀

--- src/appVersion.js
+++ src/appVersion.js
@@ -1,9 +1,9 @@
 //#region src/appVersion.ts
 var appVersion = {
-	"display": "2026.07.02-2035",
-	"sha": "1678637c",
-	"timestamp": 1783049711e3,
-	"commitUrl": "https://github.com/Stvad/knowledge-medium/commit/1678637c"
+	"display": "2026.07.03-2149",
+	"sha": "628b5086",
+	"timestamp": 178310815e4,
+	"commitUrl": "https://github.com/Stvad/knowledge-medium/commit/628b5086"
 };
 //#endregion
 export { appVersion };
--- sw.js
+++ sw.js
@@ -36,7 +36,7 @@
  */
 
 const CACHE_PREFIX = 'km-'
-const BUILD_ID = '1678637cb931'
+const BUILD_ID = '628b5086ae14'
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${BUILD_ID}`
 const ASSET_CACHE = `${CACHE_PREFIX}assets-${BUILD_ID}`
 const VENDOR_CACHE = `${CACHE_PREFIX}vendor`
