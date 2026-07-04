fcc2bff9e9dea025164c314a88846b8dd7a0446a
Stvad <Stvad@users.noreply.github.com>
2026-07-04 23:35:52 +0000

    Deploy preview for PR 321 🛫

---
+++ pr-preview/pr-321/src/appVersion.js
@@ -0,0 +1,2 @@
+var e={display:`2026.07.05-0134`,sha:`e475a76e`,timestamp:178320808e4,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/e475a76e`};export{e as appVersion};
+//# sourceMappingURL=appVersion.js.map
