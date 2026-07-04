0b0bbba494cf522f0bbc4b0f98c1f4fd90fba56e
Stvad <Stvad@users.noreply.github.com>
2026-07-04 02:28:43 +0000

    Deploy preview for PR 307 🛫

--- pr-preview/pr-307/src/appVersion.js
+++ pr-preview/pr-307/src/appVersion.js
@@ -1,9 +1,9 @@
 //#region src/appVersion.ts
 var appVersion = {
-	"display": "2026.07.04-0210",
-	"sha": "38759ae9",
-	"timestamp": 1783131006e3,
-	"commitUrl": "https://github.com/Stvad/knowledge-medium/commit/38759ae9"
+	"display": "2026.07.04-0227",
+	"sha": "518c1a4c",
+	"timestamp": 178313205e4,
+	"commitUrl": "https://github.com/Stvad/knowledge-medium/commit/518c1a4c"
 };
 //#endregion
 export { appVersion };
--- pr-preview/pr-307/sw.js
+++ pr-preview/pr-307/sw.js
@@ -36,7 +36,7 @@
  */
 
 const CACHE_PREFIX = 'km-'
-const BUILD_ID = '38759ae911ee'
+const BUILD_ID = '518c1a4cde3c'
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${BUILD_ID}`
 const ASSET_CACHE = `${CACHE_PREFIX}assets-${BUILD_ID}`
 const VENDOR_CACHE = `${CACHE_PREFIX}vendor`
