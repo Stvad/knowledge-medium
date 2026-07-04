f62ea1656496c656f5e8339cce0fd7050c039d06
Stvad <Stvad@users.noreply.github.com>
2026-07-04 03:39:13 +0000

    Remove preview for PR 307 🛬

--- pr-preview/pr-307/src/plugins/backlinks-view/facet.js
+++
@@ -1,7 +0,0 @@
-import { defineVariantFacet } from "../../facets/variantFacet.js";
-//#region src/plugins/backlinks-view/facet.ts
-var backlinksViewFacet = defineVariantFacet({ id: "backlinks-view.variants" });
-//#endregion
-export { backlinksViewFacet };
-
-//# sourceMappingURL=facet.js.map
