724a1ae4ccbdf9a0f59c081c485ff93a1311de30
Stvad <Stvad@users.noreply.github.com>
2026-07-07 00:39:41 +0000

    Deploy preview for PR 330 🛫

--- pr-preview/pr-330/sw.js
+++ pr-preview/pr-330/sw.js
@@ -336,7 +336,7 @@
 	//#endregion
 	//#region src/sw/sw.ts
 	var sw = createServiceWorker({
-		buildId: "23ff54c76cc7",
+		buildId: "16c0654abb84",
 		scopeURL: new URL(self.registration.scope),
 		keepGenerations: 3,
 		staleScopeMs: 336 * 60 * 60 * 1e3,
