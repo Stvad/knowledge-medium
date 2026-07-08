4ce6286c782ea16802346d350e4b1649d267d7d4
Vladyslav Sitalo <root@stvad.org>
2026-07-07 21:25:21 -0700

    Precache React DOM server vendor modules

--- vite-plugins/test/reactImportMapMode.test.ts
+++ vite-plugins/test/reactImportMapMode.test.ts
@@ -72,6 +72,19 @@ describe('rewriteReactImportMapForProduction', () => {
     expect(importMap.integrity[`https://esm.sh/react@${REACT_ESM_VERSION}?dev`]).toBeUndefined()
   })
 
+  it('includes the react-dom/server graph in the production integrity set', () => {
+    expect(productionReactIntegrity).toMatchObject({
+      [`https://esm.sh/react-dom@${REACT_ESM_VERSION}/server`]:
+        'sha384-tTy7T4lm9zbn4WLf9sNOE68LSnJDnyqgfNVqZpMKQubeVEmzruL9nC2qn5DovrgM',
+      [`https://esm.sh/react-dom@${REACT_ESM_VERSION}/es2022/server.mjs`]:
+        'sha384-IwzC8eRS5WH1zi7moYXhPmDA8p6w0r3uVG2yrfNRHHngpelUxeDz0raWF6O89APj',
+      [`https://esm.sh/react-dom@${REACT_ESM_VERSION}/es2022/cjs/react-dom-server-legacy.browser.production.mjs`]:
+        'sha384-RPa0ImTl9OI1lRLPTu6UD3Pi1dGOkzNqX5jmHAbzM9lGpC/4P40HuAhcXm3fIGUg',
+      [`https://esm.sh/react-dom@${REACT_ESM_VERSION}/es2022/cjs/react-dom-server.browser.production.mjs`]:
+        'sha384-4xw4onVS7+N9AtveaWuTyJlbV2W0onO6MGaTqkIUppwz4o3CDgWSwJRfv900ltXA',
+    })
+  })
+
   it('leaves unrelated import maps unchanged', () => {
     const html = `
       <script type="importmap">
