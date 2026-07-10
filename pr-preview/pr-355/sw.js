9d728867005923c5abc74630383375a7ee839b72
Stvad <Stvad@users.noreply.github.com>
2026-07-10 05:37:12 +0000

    Deploy preview for PR 355 🛫

--- pr-preview/pr-355/sw.js
+++ pr-preview/pr-355/sw.js
@@ -491,7 +491,7 @@
 	//#endregion
 	//#region src/sw/sw.ts
 	var sw = createServiceWorker({
-		buildId: "fe517cc93331",
+		buildId: "a7c24c3ffe2a",
 		scopeURL: new URL(self.registration.scope),
 		keepGenerations: 3,
 		staleScopeMs: 336 * 60 * 60 * 1e3,
