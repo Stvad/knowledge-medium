49cfd2884cf904eaede0737f2ddf95a35e6b4cc4
Stvad <Stvad@users.noreply.github.com>
2026-07-17 01:27:03 +0000

    Remove preview for PR 390 🛬

--- pr-preview/pr-390/src/appVersion.js
+++
@@ -1,2 +0,0 @@
-var e={display:`2026.07.16-2233`,sha:`e0818e7f`,timestamp:1784241196e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/e0818e7f`};export{e as appVersion};
-//# sourceMappingURL=appVersion.js.map
