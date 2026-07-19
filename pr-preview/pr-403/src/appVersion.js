d03f9ade4677c7170d23e5bdbed1f51ec46d3f29
Stvad <Stvad@users.noreply.github.com>
2026-07-19 07:33:11 +0000

    Remove preview for PR 403 🛬

--- pr-preview/pr-403/src/appVersion.js
+++
@@ -1,2 +0,0 @@
-var e={display:`2026.07.19-0342`,sha:`cc758d76`,timestamp:1784432562e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/cc758d76`};export{e as appVersion};
-//# sourceMappingURL=appVersion.js.map
--- pr-preview/pr-403/src/shortcuts/pointerAction.js
+++
@@ -1,2 +0,0 @@
-var e=null,t=t=>{e=t},n=(t,n)=>e?e(t,n):!1;export{n as dispatchPointerAction,t as setPointerActionDispatcher};
-//# sourceMappingURL=pointerAction.js.map
