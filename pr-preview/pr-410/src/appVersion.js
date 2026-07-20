bff111dadec40ceceeb132f8ba8d3d8ffcf0b7a8
Stvad <Stvad@users.noreply.github.com>
2026-07-20 22:05:10 +0000

    Deploy preview for PR 410 🛫

---
+++ pr-preview/pr-410/src/appVersion.js
@@ -0,0 +1,2 @@
+var e={display:`2026.07.21-0003`,sha:`24c9a001`,timestamp:1784585013e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/24c9a001`};export{e as appVersion};
+//# sourceMappingURL=appVersion.js.map
