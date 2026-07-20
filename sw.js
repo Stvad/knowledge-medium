c17ae5050635c85466ebff9be595f49225e0c955
Stvad <vlad@sitalo.org>
2026-07-20 23:58:24 +0000

    Deploying to gh-pages from @ Stvad/knowledge-medium@0f175cfa9920850a87d74347487e7c135e373d32 🚀

--- sw.js
+++ sw.js
@@ -491,7 +491,7 @@
 	//#endregion
 	//#region src/sw/sw.ts
 	var sw = createServiceWorker({
-		buildId: "9508efbddafb",
+		buildId: "0f175cfa9920",
 		scopeURL: new URL(self.registration.scope),
 		keepGenerations: 3,
 		staleScopeMs: 336 * 60 * 60 * 1e3,
