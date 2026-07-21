0f71c5cf0e2369a6c620a10795da341fcb988e15
Stvad <Stvad@users.noreply.github.com>
2026-07-21 20:50:07 +0000

    Remove preview for PR 416 🛬

--- pr-preview/pr-416/src/appVersion.js
+++
@@ -1,2 +0,0 @@
-var e={display:`2026.07.21-0211`,sha:`47f965ed`,timestamp:1784599883e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/47f965ed`};export{e as appVersion};
-//# sourceMappingURL=appVersion.js.map
--- pr-preview/pr-416/src/data/api/blockType.js
+++
@@ -1,2 +0,0 @@
-var e={hideFromCompletion:!0,hideFromBlockDisplay:!0};export{e as INFRASTRUCTURE_TYPE_DISPLAY};
-//# sourceMappingURL=blockType.js.map
