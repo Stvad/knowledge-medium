2a5dbf085f433d118869e486ec1a79babc1dd5bd
Stvad <Stvad@users.noreply.github.com>
2026-07-29 08:32:06 +0000

    Deploy preview for PR 427 🛫

--- pr-preview/pr-427/src/appVersion.js
+++ pr-preview/pr-427/src/appVersion.js
@@ -1,2 +1,2 @@
-var e={display:`2026.07.23-0812`,sha:`bd6ae2b6`,timestamp:1784794356e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/bd6ae2b6`};export{e as appVersion};
+var e={display:`2026.07.29-1031`,sha:`e0889abc`,timestamp:1785313869e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/e0889abc`};export{e as appVersion};
 //# sourceMappingURL=appVersion.js.map
