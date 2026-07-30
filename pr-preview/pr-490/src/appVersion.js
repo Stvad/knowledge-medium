99271c66957e23e46d36fa35cd6a872d1fab8cce
Stvad <Stvad@users.noreply.github.com>
2026-07-30 19:29:45 +0000

    Remove preview for PR 490 🛬

--- pr-preview/pr-490/src/appVersion.js
+++
@@ -1,2 +0,0 @@
-var e={display:`2026.07.30-1917`,sha:`9772f797`,timestamp:1785439044e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/9772f797`};export{e as appVersion};
-//# sourceMappingURL=appVersion.js.map
