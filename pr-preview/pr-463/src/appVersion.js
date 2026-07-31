3c3490d4f8d7e1fb9133c0253e8c00b08b225417
Stvad <Stvad@users.noreply.github.com>
2026-07-31 19:38:34 +0000

    Remove preview for PR 463 🛬

--- pr-preview/pr-463/src/appVersion.js
+++
@@ -1,2 +0,0 @@
-var e={display:`2026.07.31-1859`,sha:`a3db74d7`,timestamp:1785524378e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/a3db74d7`};export{e as appVersion};
-//# sourceMappingURL=appVersion.js.map
