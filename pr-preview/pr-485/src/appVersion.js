140ddf0068ab0cd074860f37e0d682c1b1dea7d7
Stvad <Stvad@users.noreply.github.com>
2026-07-31 18:46:26 +0000

    Remove preview for PR 485 🛬

--- pr-preview/pr-485/src/appVersion.js
+++
@@ -1,2 +0,0 @@
-var e={display:`2026.07.30-1038`,sha:`92c317a5`,timestamp:1785400732e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/92c317a5`};export{e as appVersion};
-//# sourceMappingURL=appVersion.js.map
