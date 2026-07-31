6116cedcf84939498a0c86f59a747c49a8eb8c6e
Stvad <Stvad@users.noreply.github.com>
2026-07-31 06:24:04 +0000

    Deploy preview for PR 513 🛫

--- pr-preview/pr-513/src/appVersion.js
+++ pr-preview/pr-513/src/appVersion.js
@@ -1,2 +1,2 @@
-var e={display:`2026.07.31-0612`,sha:`ff41fd58`,timestamp:1785478329e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/ff41fd58`};export{e as appVersion};
+var e={display:`2026.07.31-0622`,sha:`5ebb64bb`,timestamp:1785478971e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/5ebb64bb`};export{e as appVersion};
 //# sourceMappingURL=appVersion.js.map
