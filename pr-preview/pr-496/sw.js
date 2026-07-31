ecfe0c312434147ae6f9b893855d9bcffa3d7de4
Stvad <Stvad@users.noreply.github.com>
2026-07-31 01:51:43 +0000

    Deploy preview for PR 496 🛫

--- pr-preview/pr-496/sw.js
+++ pr-preview/pr-496/sw.js
@@ -491,7 +491,7 @@
 	//#endregion
 	//#region src/sw/sw.ts
 	var sw = createServiceWorker({
-		buildId: "1672a791908f",
+		buildId: "c13dd7889fa7",
 		scopeURL: new URL(self.registration.scope),
 		keepGenerations: 3,
 		staleScopeMs: 336 * 60 * 60 * 1e3,
