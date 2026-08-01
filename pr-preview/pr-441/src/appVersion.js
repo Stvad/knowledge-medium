c1685424178b04278a67d2e6ad265adb901b9606
Stvad <Stvad@users.noreply.github.com>
2026-08-01 12:00:34 +0000

    Deploy preview for PR 441 🛫

--- pr-preview/pr-441/src/appVersion.js
+++ pr-preview/pr-441/src/appVersion.js
@@ -1,2 +1,2 @@
-var e={display:`2026.08.01-1139`,sha:`66ca9fac`,timestamp:178558435e4,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/66ca9fac`};export{e as appVersion};
+var e={display:`2026.08.01-1159`,sha:`9a359a8a`,timestamp:1785585561e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/9a359a8a`};export{e as appVersion};
 //# sourceMappingURL=appVersion.js.map
--- pr-preview/pr-441/sw.js
+++ pr-preview/pr-441/sw.js
@@ -491,7 +491,7 @@
 	//#endregion
 	//#region src/sw/sw.ts
 	var sw = createServiceWorker({
-		buildId: "66ca9facc4b1",
+		buildId: "9a359a8add5e",
 		scopeURL: new URL(self.registration.scope),
 		keepGenerations: 3,
 		staleScopeMs: 336 * 60 * 60 * 1e3,
