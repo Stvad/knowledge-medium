c70b14ee9b9a46633f7bec365747b5a1bed5ed83
Stvad <Stvad@users.noreply.github.com>
2026-08-04 17:34:11 +0000

    Remove preview for PR 503 🛬

--- pr-preview/pr-503/src/appVersion.js
+++
@@ -1,2 +0,0 @@
-var e={display:`2026.08.04-0801`,sha:`e2dc42c2`,timestamp:1785830472e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/e2dc42c2`};export{e as appVersion};
-//# sourceMappingURL=appVersion.js.map
