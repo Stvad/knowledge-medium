d09ec44d1413ffded7109276342c0d2a4327bbab
Stvad <vlad@sitalo.org>
2026-08-14 17:49:09 +0000

    Deploying to gh-pages from @ Stvad/knowledge-medium@70c9d86244c3bae137a8c53cddc41ecb4a07929a 🚀

--- src/appVersion.js
+++ src/appVersion.js
@@ -1,2 +1,2 @@
-var e={display:`2026.08.14-1022`,sha:`830169a9`,timestamp:1786695769e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/830169a9`};export{e as appVersion};
+var e={display:`2026.08.14-1947`,sha:`70c9d862`,timestamp:1786729667e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/70c9d862`};export{e as appVersion};
 //# sourceMappingURL=appVersion.js.map
--- sw.js
+++ sw.js
@@ -491,7 +491,7 @@
 	//#endregion
 	//#region src/sw/sw.ts
 	var sw = createServiceWorker({
-		buildId: "830169a94c67",
+		buildId: "70c9d86244c3",
 		scopeURL: new URL(self.registration.scope),
 		keepGenerations: 3,
 		staleScopeMs: 336 * 60 * 60 * 1e3,
