979d9030e65754fe33427b477604b923f1b53a93
Stvad <Stvad@users.noreply.github.com>
2026-08-14 02:25:17 +0000

    Deploy preview for PR 549 🛫

--- pr-preview/pr-549/src/appVersion.js
+++ pr-preview/pr-549/src/appVersion.js
@@ -1,2 +1,2 @@
-var e={display:`2026.08.14-0208`,sha:`e755d591`,timestamp:1786673321e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/e755d591`};export{e as appVersion};
+var e={display:`2026.08.14-0224`,sha:`46caac23`,timestamp:1786674242e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/46caac23`};export{e as appVersion};
 //# sourceMappingURL=appVersion.js.map
--- pr-preview/pr-549/sw.js
+++ pr-preview/pr-549/sw.js
@@ -491,7 +491,7 @@
 	//#endregion
 	//#region src/sw/sw.ts
 	var sw = createServiceWorker({
-		buildId: "e755d591c2bb",
+		buildId: "46caac23259a",
 		scopeURL: new URL(self.registration.scope),
 		keepGenerations: 3,
 		staleScopeMs: 336 * 60 * 60 * 1e3,
