9808bdae908c7e811a16b22701471eac05a44cc6
Stvad <Stvad@users.noreply.github.com>
2026-08-14 22:45:28 +0000

    Deploy preview for PR 544 🛫

--- pr-preview/pr-544/src/appVersion.js
+++ pr-preview/pr-544/src/appVersion.js
@@ -1,2 +1,2 @@
-var e={display:`2026.08.14-0626`,sha:`dd08ff07`,timestamp:1786688792e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/dd08ff07`};export{e as appVersion};
+var e={display:`2026.08.14-2244`,sha:`aaa5c78d`,timestamp:1786747454e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/aaa5c78d`};export{e as appVersion};
 //# sourceMappingURL=appVersion.js.map
