3712e75d6bbe11687cbf22f08487aee037b5636c
Stvad <Stvad@users.noreply.github.com>
2026-08-17 07:35:09 +0000

    Deploy preview for PR 577 🛫

--- pr-preview/pr-577/src/appVersion.js
+++ pr-preview/pr-577/src/appVersion.js
@@ -1,2 +1,2 @@
-var e={display:`2026.08.17-0718`,sha:`d2f127b6`,timestamp:1786951094e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/d2f127b6`};export{e as appVersion};
+var e={display:`2026.08.17-0734`,sha:`288ec65b`,timestamp:1786952045e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/288ec65b`};export{e as appVersion};
 //# sourceMappingURL=appVersion.js.map
