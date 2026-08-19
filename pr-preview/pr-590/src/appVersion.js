f5a651e15bb57c3464e5757284b87c6fea16a388
Stvad <Stvad@users.noreply.github.com>
2026-08-19 00:49:52 +0000

    Deploy preview for PR 590 🛫

---
+++ pr-preview/pr-590/src/appVersion.js
@@ -0,0 +1,2 @@
+var e={display:`2026.08.19-0248`,sha:`6c15c49a`,timestamp:1787100525e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/6c15c49a`};export{e as appVersion};
+//# sourceMappingURL=appVersion.js.map
