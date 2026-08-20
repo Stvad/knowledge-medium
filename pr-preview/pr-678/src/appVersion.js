e9e5936a46b280fc0d10e01afeae2e3fd58a6dd4
Stvad <Stvad@users.noreply.github.com>
2026-08-20 22:44:39 +0000

    Deploy preview for PR 678 🛫

---
+++ pr-preview/pr-678/src/appVersion.js
@@ -0,0 +1,2 @@
+var e={display:`2026.08.21-0043`,sha:`24f7ae2b`,timestamp:1787265811e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/24f7ae2b`};export{e as appVersion};
+//# sourceMappingURL=appVersion.js.map
