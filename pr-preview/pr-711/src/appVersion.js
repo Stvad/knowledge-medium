56cb0961f559f202ba6ea6a1f531f8bd7a6e7ca3
Stvad <Stvad@users.noreply.github.com>
2026-08-21 20:41:43 +0000

    Deploy preview for PR 711 🛫

--- pr-preview/pr-711/src/appVersion.js
+++ pr-preview/pr-711/src/appVersion.js
@@ -1,2 +1,2 @@
-var e={display:`2026.08.21-2019`,sha:`37df22e9`,timestamp:1787343559e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/37df22e9`};export{e as appVersion};
+var e={display:`2026.08.21-2040`,sha:`8c30ef81`,timestamp:1787344825e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/8c30ef81`};export{e as appVersion};
 //# sourceMappingURL=appVersion.js.map
--- pr-preview/pr-711/sw.js
+++ pr-preview/pr-711/sw.js
@@ -491,7 +491,7 @@
 	//#endregion
 	//#region src/sw/sw.ts
 	var sw = createServiceWorker({
-		buildId: "37df22e9b578",
+		buildId: "8c30ef81756d",
 		scopeURL: new URL(self.registration.scope),
 		keepGenerations: 3,
 		staleScopeMs: 336 * 60 * 60 * 1e3,
