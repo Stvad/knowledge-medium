9a0fc7b843ff89e0e02fbb2c730b838579b7b00e
Stvad <Stvad@users.noreply.github.com>
2026-08-24 22:57:57 +0000

    Remove preview for PR 830 🛬

--- pr-preview/pr-830/src/appVersion.js
+++
@@ -1,2 +0,0 @@
-var e={display:`2026.08.24-2251`,sha:`c0d28f84`,timestamp:1787611866e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/c0d28f84`};export{e as appVersion};
-//# sourceMappingURL=appVersion.js.map
