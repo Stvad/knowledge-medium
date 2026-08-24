d4dafcbdcca2fde1bcad856ddcb9cd9737a00f70
Stvad <Stvad@users.noreply.github.com>
2026-08-24 05:55:11 +0000

    Remove preview for PR 770 🛬

--- pr-preview/pr-770/src/appVersion.js
+++
@@ -1,2 +0,0 @@
-var e={display:`2026.08.24-0511`,sha:`bad7360a`,timestamp:1787548283e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/bad7360a`};export{e as appVersion};
-//# sourceMappingURL=appVersion.js.map
