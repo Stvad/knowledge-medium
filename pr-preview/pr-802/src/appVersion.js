2b054b48e36af278afe11672e0a80c83d8684111
Stvad <Stvad@users.noreply.github.com>
2026-08-24 05:08:17 +0000

    Deploy preview for PR 802 🛫

---
+++ pr-preview/pr-802/src/appVersion.js
@@ -0,0 +1,2 @@
+var e={display:`2026.08.24-0707`,sha:`410cd455`,timestamp:1787548021e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/410cd455`};export{e as appVersion};
+//# sourceMappingURL=appVersion.js.map
