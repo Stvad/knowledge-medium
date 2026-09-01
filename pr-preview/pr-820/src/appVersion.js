9a07ca20e979f5d68b1d538ed88c42188281d6bc
Stvad <Stvad@users.noreply.github.com>
2026-09-01 04:39:39 +0000

    Remove preview for PR 820 🛬

--- pr-preview/pr-820/src/appVersion.js
+++
@@ -1,2 +0,0 @@
-var e={display:`2026.09.01-0313`,sha:`1307be39`,timestamp:1788232395e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/1307be39`};export{e as appVersion};
-//# sourceMappingURL=appVersion.js.map
--- pr-preview/pr-820/src/data/userPrefs.js
+++
@@ -1,2 +0,0 @@
-var e=`user-prefs`,t=`ui-state`,n=[t,e];export{t as UI_STATE_PATH_PART,e as USER_PREFS_PATH_PART,n as USER_STATE_ROOT_PATHS};
-//# sourceMappingURL=userPrefs.js.map
