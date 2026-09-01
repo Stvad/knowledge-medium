d34f180e50cbd060f7f8d1de6c8f22bebaf7d2bd
Stvad <Stvad@users.noreply.github.com>
2026-09-01 22:51:59 +0000

    Deploy preview for PR 862 🛫

--- pr-preview/pr-862/src/appVersion.js
+++ pr-preview/pr-862/src/appVersion.js
@@ -1,2 +1,2 @@
-var e={display:`2026.09.01-2058`,sha:`b86ddb23`,timestamp:1788296295e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/b86ddb23`};export{e as appVersion};
+var e={display:`2026.09.01-1550`,sha:`5e8aa2ed`,timestamp:1788303045e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/5e8aa2ed`};export{e as appVersion};
 //# sourceMappingURL=appVersion.js.map
