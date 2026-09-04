4784795e9f3725e44ff805939d51e260e6599340
Stvad <Stvad@users.noreply.github.com>
2026-09-04 20:48:41 +0000

    Remove preview for PR 839 🛬

--- pr-preview/pr-839/src/appVersion.js
+++
@@ -1,2 +0,0 @@
-var e={display:`2026.09.03-2113`,sha:`8b7fb58e`,timestamp:1788470023e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/8b7fb58e`};export{e as appVersion};
-//# sourceMappingURL=appVersion.js.map
