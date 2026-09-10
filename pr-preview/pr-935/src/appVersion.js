9fda0ae6c9f789a039b9cac141e56f02b6ded332
Stvad <Stvad@users.noreply.github.com>
2026-09-10 23:52:04 +0000

    Deploy preview for PR 935 🛫

--- pr-preview/pr-935/src/appVersion.js
+++ pr-preview/pr-935/src/appVersion.js
@@ -1,2 +1,2 @@
-var e={display:`2026.09.10-2341`,sha:`19bc91e6`,timestamp:1789083681e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/19bc91e6`};export{e as appVersion};
+var e={display:`2026.09.10-2351`,sha:`615099ce`,timestamp:1789084262e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/615099ce`};export{e as appVersion};
 //# sourceMappingURL=appVersion.js.map
