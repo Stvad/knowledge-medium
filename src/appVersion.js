2dfed11c8ba06679907619a10421d983f4e7fd83
Stvad <vlad@sitalo.org>
2026-09-11 18:54:03 +0000

    Deploying to gh-pages from @ Stvad/knowledge-medium@bd6832cd60248071afd3debe6c124d2256ad449a 🚀

--- src/appVersion.js
+++ src/appVersion.js
@@ -1,2 +1,2 @@
-var e={display:`2026.09.11-2029`,sha:`c932e057`,timestamp:1789151378e3,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/c932e057`};export{e as appVersion};
+var e={display:`2026.09.11-2052`,sha:`bd6832cd`,timestamp:178915276e4,commitUrl:`https://github.com/Stvad/knowledge-medium/commit/bd6832cd`};export{e as appVersion};
 //# sourceMappingURL=appVersion.js.map
